b0baa3ff48cce42267ce5f462af20f6cfcf7d64e
Hannah Stahl <hls62@cornell.edu>
2020-01-28 09:12:15 -0600

    Added productsToTags table

---
+++ productsToTags/create.js
@@ -0,0 +1,23 @@
+import uuid from "uuid";
+import * as dynamoDbLib from "../libs/dynamodb-lib";
+import { success, failure } from "../libs/response-lib";
+
+export async function main(event, context) {
+  const data = JSON.parse(event.body);
+  const params = {
+    TableName: process.env.productToTagTableName,
+    Item: {
+      userId: event.requestContext.identity.cognitoIdentityId,
+      productId: data.productId,
+      tagId: data.tagId,
+      createdAt: Date.now()
+    }
+  };
+
+  try {
+    await dynamoDbLib.call("put", params);
+    return success(params.Item);
+  } catch (error) {
+    return failure({ status: false, error });
+  }
+}
