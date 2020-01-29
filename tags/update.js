80542edc373ea82dfac3898400107577eb38c30a
Hannah Stahl <hls62@cornell.edu>
2020-01-29 08:31:47 -0600

    Converted update function into a generic, reusable function for other fields besides tags

--- tags/update.js
+++ tags/update.js
@@ -1,99 +1,12 @@
-import uuid from "uuid";
-import * as dynamoDbLib from "../libs/dynamodb-lib";
-import { success, failure } from "../libs/response-lib";
+import { update } from '../update';
 
 export async function main(event, context) {
   const data = JSON.parse(event.body);
-  const { productId, selectedTagIds } = data;
-  const userId = event.requestContext.identity.cognitoIdentityId;
-
-  try {
-    // Add any new tags that were created to tags table
-    const result = await dynamoDbLib.call("query", {
-      TableName: process.env.tagTableName,
-      KeyConditionExpression: "userId = :userId",
-      ExpressionAttributeValues: { ":userId": userId }
-    });
-    const existingTagIds = result.Items.map(tag => tag.tagId);
-    let promises = [];
-    selectedTagIds.forEach((tagId, index) => {
-      if (!existingTagIds.includes(tagId)) {
-        const newTag = {
-          userId,
-          tagId: uuid.v1(),
-          tagName: tagId,
-          createdAt: Date.now(),
-        };
-        selectedTagIds[index] = newTag.tagId;
-        promises.push(dynamoDbLib.call("put", {
-          TableName: process.env.tagTableName,
-          Item: newTag,
-        }));
-      }
-    });
-
-    // Delete any removed productToTag relationships for this product from productToTags table
-    const result2 = await dynamoDbLib.call("query", {
-      TableName: process.env.productToTagTableName,
-      KeyConditionExpression: "userId = :userId",
-      FilterExpression: "productId = :productId",
-      ExpressionAttributeValues: { ":userId": userId, ":productId": productId }
-    });
-    const existingProductToTags = result2.Items;
-    existingProductToTags.forEach((productToTag) => {
-      if (!selectedTagIds.includes(productToTag.tagId)) {
-        promises.push(dynamoDbLib.call("delete", {
-          TableName: process.env.productToTagTableName,
-          Key: { userId, productToTagId: productToTag.productToTagId }
-        }));
-      }
-    });
-
-    // Add any new productToTag relationships for this product to productToTags table
-    const existingTagIdsForProduct = existingProductToTags.map(productToTag => productToTag.tagId);
-    selectedTagIds.forEach((tagId) => {
-      if (!existingTagIdsForProduct.includes(tagId)) {
-        promises.push(dynamoDbLib.call("put", {
-          TableName: process.env.productToTagTableName,
-          Item: {
-            userId,
-            productToTagId: uuid.v1(),
-            productId,
-            tagId,
-            createdAt: Date.now()
-          }
-        }));
-      }
-    });
-    await Promise.all(promises);
-
-    // Delete any no-longer-used tags from tags table
-    const [result3, result4] = await Promise.all([
-      dynamoDbLib.call("query", {
-        TableName: process.env.tagTableName,
-        KeyConditionExpression: "userId = :userId",
-        ExpressionAttributeValues: { ":userId": userId }
-      }),
-      dynamoDbLib.call("query", {
-        TableName: process.env.productToTagTableName,
-        KeyConditionExpression: "userId = :userId",
-        ExpressionAttributeValues: { ":userId": userId }
-      }),
-    ]);
-    const tags = result3.Items;
-    const usedTagIds = result4.Items.map(productToTag => productToTag.tagId);
-    const tagIdsToRemove = tags.map(tag => tag.tagId).filter(tagId => !usedTagIds.includes(tagId));
-    promises = [];
-    tagIdsToRemove.forEach((tagId) => {
-      promises.push(dynamoDbLib.call("delete", {
-        TableName: process.env.tagTableName,
-        Key: { userId, tagId }
-      }));
-    });
-    await Promise.all(promises);
-
-    return success({ status: true });
-  } catch (error) {
-    return failure({ status: false, error });
-  }
+  const result = await update({
+    userId: event.requestContext.identity.cognitoIdentityId,
+    productId: data.productId,
+    selectedIds: data.selectedTagIds,
+    itemType: 'tag',
+  });
+  return result;
 }
---
+++ update.js
@@ -0,0 +1,98 @@
+import uuid from "uuid";
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
+
+function capitalize(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+export async function update({ userId, productId, selectedIds, itemType }) {
+  try {
+    // Add any new values that were created
+    const tableName = process.env[`${itemType}TableName`];
+    const joiningTableName = process.env[`productTo${capitalize(itemType)}TableName`];
+    const result = await dynamoDbLib.call("query", {
+      TableName: tableName,
+      KeyConditionExpression: "userId = :userId",
+      ExpressionAttributeValues: { ":userId": userId }
+    });
+    const existingIds = result.Items.map(item => item[`${itemType}Id`]);
+    let promises = [];
+    selectedIds.forEach((id, index) => {
+      if (!existingIds.includes(id)) {
+        const newItem = { userId, createdAt: Date.now() };
+        newItem[`${itemType}Id`] = uuid.v1();
+        newItem[`${itemType}Name`] = id;
+        selectedIds[index] = newItem[`${itemType}Id`];
+        promises.push(dynamoDbLib.call("put", {
+          TableName: tableName, Item: newItem,
+        }));
+      }
+    });
+
+    // Delete any removed relationships to this product
+    const result2 = await dynamoDbLib.call("query", {
+      TableName: joiningTableName,
+      KeyConditionExpression: "userId = :userId",
+      FilterExpression: "productId = :productId",
+      ExpressionAttributeValues: { ":userId": userId, ":productId": productId }
+    });
+    const existingRelationships = result2.Items;
+    existingRelationships.forEach((relationship) => {
+      if (!selectedIds.includes(relationship[`${itemType}Id`])) {
+        const key = { userId };
+        key[`productTo${capitalize(itemType)}Id`] = relationship[`productTo${capitalize(itemType)}Id`];
+        promises.push(dynamoDbLib.call("delete", {
+          TableName: joiningTableName,
+          Key: key,
+        }));
+      }
+    });
+
+    // Add any new relationships to this product
+    const existingIdsForProduct = existingRelationships.map(relationship => relationship[`${itemType}Id`]);
+    selectedIds.forEach((id) => {
+      if (!existingIdsForProduct.includes(id)) {
+        const item = { userId, productId, createdAt: Date.now() };
+        item[`productTo${capitalize(itemType)}Id`] = uuid.v1();
+        item[`${itemType}Id`] = id;
+        promises.push(dynamoDbLib.call("put", {
+          TableName: joiningTableName,
+          Item: item,
+        }));
+      }
+    });
+    await Promise.all(promises);
+
+    // Delete any no-longer-used values
+    const [result3, result4] = await Promise.all([
+      dynamoDbLib.call("query", {
+        TableName: tableName,
+        KeyConditionExpression: "userId = :userId",
+        ExpressionAttributeValues: { ":userId": userId }
+      }),
+      dynamoDbLib.call("query", {
+        TableName: joiningTableName,
+        KeyConditionExpression: "userId = :userId",
+        ExpressionAttributeValues: { ":userId": userId }
+      }),
+    ]);
+    const items = result3.Items;
+    const usedIds = result4.Items.map(relationship => relationship[`${itemType}Id`]);
+    const idsToRemove = items.map(item => item[`${itemType}Id`]).filter(id => !usedIds.includes(id));
+    promises = [];
+    idsToRemove.forEach((id) => {
+      const key = { userId };
+      key[`${itemType}Id`] = id;
+      promises.push(dynamoDbLib.call("delete", {
+        TableName: tableName,
+        Key: key,
+      }));
+    });
+    await Promise.all(promises);
+
+    return success({ status: true });
+  } catch (error) {
+    return failure({ status: false, error });
+  }
+}
